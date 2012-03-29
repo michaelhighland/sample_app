ec01067138fbff77580384e1d491b4d4d0086fac
Michael Highland <michael@iamfutureproof.com>
2012-03-29 14:08:15 -0400

    full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,10 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
   end
 
@@ -23,7 +23,7 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Help')
     end
 
-    it "should have the title 'Help'" do
+    it "should have the base title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Help")
@@ -43,4 +43,19 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+  
+  describe "Contact page" do
+
+      it "should have the h1 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_selector('h1', :text => 'Contact')
+      end
+
+      it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+      end
+  end
+  
+  
 end
